3891766df2fa122911b37272dc8c61c032c68f88
Celyn Walters <3299161+celynw@users.noreply.github.com>
2022-08-30 20:40:49 +0100

    Add 'Files' from chocolatey page for version 1.3.1

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference	= 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageName	= 'whatsapptray'
+$url32			  = 'https://github.com/D4koon/WhatsappTray/releases/download/1.3.1/WhatsappTrayV1.3.1.exe'
+$checksum32		= 'DB95B366751CC7EFF056246C891DE0F418DB9A633FD224011546707E45B575DD'
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'exe'
+  url           = $url32
+  softwareName  = 'whatsapptray*'
+  checksum      = $checksum32
+  checksumType  = 'sha256'
+  silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
+}
+
+Install-ChocolateyPackage @packageArgs
