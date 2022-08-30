bd6c77ac6675d95d8b5d22d2b6ceac2a5a5c30cd
Celyn Walters <3299161+celynw@users.noreply.github.com>
2022-08-30 20:42:00 +0100

    Add files for version 1.9.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,18 +1,16 @@
-﻿$ErrorActionPreference	= 'Stop';
+﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-
-$packageName	= 'whatsapptray'
-$url32			  = 'https://github.com/D4koon/WhatsappTray/releases/download/1.3.1/WhatsappTrayV1.3.1.exe'
-$checksum32		= 'DB95B366751CC7EFF056246C891DE0F418DB9A633FD224011546707E45B575DD'
+$url = 'https://github.com/D4koon/WhatsappTray/releases/download/v1.9.0/WhatsappTrayV1.9.0.0.exe'
 
 $packageArgs = @{
-  packageName   = $packageName
-  fileType      = 'exe'
-  url           = $url32
-  softwareName  = 'whatsapptray*'
-  checksum      = $checksum32
-  checksumType  = 'sha256'
-  silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
+	packageName = $env:ChocolateyPackageName
+	unzipLocation = $toolsDir
+	fileType = 'exe'
+	url = $url
+	softwareName = 'whatsapptray'
+	checksum = '6535FA20F12FD21C0ABD104E5594D4F0AE82528D4274D3470D9C1FBB3C403A18'
+	checksumType = 'sha256'
+	silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
 }
 
 Install-ChocolateyPackage @packageArgs
